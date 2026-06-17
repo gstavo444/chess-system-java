5b030403991e89ea64dc8b99b0a837baf6c1fd53
gustavo costa <gusta638costa@gmail.com>
2026-06-16 23:05:47 -0300

    Fix board printing improvement

--- src/application/UI.java
+++ src/application/UI.java
@@ -42,13 +42,6 @@ public class UI {
             System.out.print("-");
         }
         else {
-            System.out.print(piece);
-        }
-        System.out.print(" ");
-        if (piece == null) {
-            System.out.print("-");
-        }
-        else {
             if (piece.getColor() == Color.WHITE) {
                 System.out.print(ANSI_WHITE + piece + ANSI_RESET);
             }
@@ -58,4 +51,5 @@ public class UI {
         }
         System.out.print(" ");
     }
-}
+  }
+
